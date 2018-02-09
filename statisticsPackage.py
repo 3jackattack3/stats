bb3d45480a9892b6f435669eba106522a7c866ba
Jack Defay <3jackattack3@gmail.com>
2018-02-09 10:21:27 -0500

    wrote averaging method

--- statisticsPackage.py
+++ statisticsPackage.py
@@ -3,8 +3,11 @@ import sys
 def add(num1, num2):
     return num1+num2
 
-def average(num1, num2):
-    return (num1+num2)/2
+def average(list):
+    mean = 0
+    for counter in range(len(list)):
+        mean+=list[counter]
+    return mean/len(list)
 
-print(add(2,2))
-print(average(2,4))
+print(add(2,6))
+print(average([4,4,6,10]))
