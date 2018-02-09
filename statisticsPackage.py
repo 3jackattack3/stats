d4b1f4179b83fc0f7629b0eaa90e8452f123dafb
Jack Defay <3jackattack3@gmail.com>
2018-02-09 10:05:37 -0500

    troubleshooting

--- statisticsPackage.py
+++ statisticsPackage.py
@@ -3,13 +3,8 @@ import sys
 def add(num1, num2):
     return num1+num2
 
-def mean(list):
-    mean = 0
-    #for counter in range(list):
-    #    mean+=list[counter]
-    #mean/=list.length
-    return mean
+def average(num1, num2):
+    return (num1+num2)/2
 
 print(add(2,2))
-list = {2,4}
-print(mean(list))
+print(average(2,4))
