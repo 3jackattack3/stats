104dec3f5d919798771b1d45820bfb6f24a88584
Jack Defay <3jackattack3@gmail.com>
2018-02-09 10:01:31 -0500

    added import sys to top, fixed error

--- statisticsPackage.py
+++ statisticsPackage.py
@@ -1,4 +1,15 @@
+import sys
+
 def add(num1, num2):
     return num1+num2
 
-print(add(2,2,))
+def mean(list):
+    mean = 0
+    #for counter in range(list):
+    #    mean+=list[counter]
+    #mean/=list.length
+    return mean
+
+print(add(2,2))
+list = {2,4}
+print(mean(list))
