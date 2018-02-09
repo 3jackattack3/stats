a0146e303252e1d5e63dfa0f6669819973015b8e
Jack <3jackattack3@gmail.com>
2018-02-09 12:05:47 -0500

    idk

--- statisticsPackage.py
+++ statisticsPackage.py
@@ -7,5 +7,12 @@ def average(list):
         mean+=list[counter]
     return mean/len(list)
 
+def median(list):
+    list.sort()
+    if(len(list)%2 = 1):
+        return list[(len(list)+1)/2]
+    else:
+        return average(list[len(list)/2], list[len(list)/2+1])
+
 print(add(2,6))
 print(average([4,4,6,10]))
