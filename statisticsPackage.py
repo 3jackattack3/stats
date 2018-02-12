e9d8111106dd5eed77c299a2cce74ab70114e2b6
Jack <3jackattack3@gmail.com>
2018-02-12 14:17:55 -0500

    still working

--- statisticsPackage.py
+++ statisticsPackage.py
@@ -9,12 +9,27 @@ def average(list):
 
 def median(list):
     list.sort()
-    if(len(list)%2 = 1):
-        return list[(len(list)+1)/2]
+    if(len(list)%2 == 1):
+        return list[(int) ((len(list))/2)]
     else:
-        return average(list[len(list)/2], list[len(list)/2+1])
+        return average([list[(int) (len(list)/2)], list[(int) (len(list)/2-1)]])
 
-        #hello!
+def mode(list):
+    longestStreak = 0
+    currentStreak = 0
+
+    for counter in range(len(list)):
+        if(list[counter] != list[counter+1]):
+
+        if(longestStreak < currentStreak):
+            longestStreak = currentStreak
+
+def range:
+
+def std:
+
+
 
 print(add(2,6))
-print(average([4,4,6,10]))
+print(average([4,4,6,14]))
+print(median([1,3,4,5,6]))
