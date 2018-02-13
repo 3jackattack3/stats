d573337d60ab2ed0fd887d812771e7c503912d87
Jack <3jackattack3@gmail.com>
2018-02-13 08:30:15 -0500

    created mode method

--- statisticsPackage.py
+++ statisticsPackage.py
@@ -17,19 +17,25 @@ def median(list):
 def mode(list):
     longestStreak = 0
     currentStreak = 0
+    streakValue = 0
 
-    for counter in range(len(list)):
+    for counter in range(len(list)-1):
         if(list[counter] != list[counter+1]):
-
+            currentStreak = 0
         if(longestStreak < currentStreak):
             longestStreak = currentStreak
+            streakValue = list[counter]
+        currentStreak+=1
 
-def range:
+    return streakValue
 
-def std:
+#def range:
+
+#def std:
 
 
 
 print(add(2,6))
 print(average([4,4,6,14]))
 print(median([1,3,4,5,6]))
+print(mode([1,2,2,2]))
